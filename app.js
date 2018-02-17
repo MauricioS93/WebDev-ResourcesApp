732c51e6156d817c2c8616f24f4690dc52eb53fd
Mauricio Sarmiento <micho.sarmiento.b@gmail.com>
2018-02-16 22:28:31 -0800

    routes REST ready and dropdown menu

--- app.js
+++ app.js
@@ -13,19 +13,12 @@ let Blog = require("./models/blogs");
 let blogRoute = require("./routes/blogs");
 
 // App configuration
-mongoose.connect("mongodb://localhost/todolist");
+mongoose.connect("mongodb://localhost/resourcesApp");
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true}));
-app.use(expressSanitizer());
+app.use(expressSanitizer()); //Hast to go before bodyParser
 app.use(methodOverride("_method"));
-
-// create a blog for now
-// Blog.create({
-//   title: 'Test one',
-//   image: 'https://images.unsplash.com/photo-1470138831303-3e77dd49163e?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=3e4426f6ac48656306b05756326613c5&auto=format&fit=crop&w=1050&q=80',
-//   body: 'this is just a test '
-// });
 
 // Tell App to use those routes created.
 app.use(blogRoute);
--- routes/blogs.js
+++ routes/blogs.js
@@ -29,8 +29,8 @@ router.get("/blogs/new", (req, res) => {
 
 // Create a new blog
 router.post("/blogs", (req, res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, (err, newBlog) => {
-        // console.log(req.body.blog);
         if(err){
             res.render('new');
         } else {
@@ -63,11 +63,23 @@ router.get('/blogs/:id/edit', (req,res) => {
 
 // Update Route - Send information from form and update database
 router.put('/blogs/:id', (req, res) => {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedPost) => {
         if(err) {
             res.redirect('back');
         } else {
             res.redirect('/blogs/' + req.params.id);
+        }
+    });
+});
+
+// Remove Route
+router.delete('/blogs/:id', (req, res) => {
+    Blog.findByIdAndRemove(req.params.id, err => {
+        if (err){
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs');
         }
     });
 });
