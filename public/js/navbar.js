bd5ef9f3cb77b8e40640ec78f33c3ecd17452100
Mauricio Sarmiento <micho.sarmiento.b@gmail.com>
2018-02-22 16:29:32 -0800

    fixed javascript and add currentUser.username

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,10 +1,12 @@
-$(document).ready(function(){
-    $('.ui.left.sidebar').sidebar({
-        transition: 'overlay'
-    });
-    // left is opened by button
-    $('.ui.left.sidebar')
-        .sidebar('attach events', '.open.button');
-    $('.ui .dropdown').dropdown();
+$('.ui.left.sidebar').sidebar({
+    transition: 'overlay'
 });
+
+// Sidebar opens when clicked
+$('#toggle').click(()=>{
+    $('.ui.sidebar').sidebar('toggle');
+});
+
+//Inside menu dropdown
+$('.ui .dropdown').dropdown();
 
